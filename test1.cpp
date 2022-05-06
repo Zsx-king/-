29d7c11fb540f5a1d761777e2fb5b3c984834a30
Zsx-king <105053691+Zsx-king@users.noreply.github.com>
2022-05-06 20:02:37 +0800

    Add files via upload

---
+++ test1.cpp
@@ -0,0 +1,266 @@
+//# include <stdio.h>
+//int main()
+//{
+	//char c1,c2;
+	//a=10;
+	//b=3;
+	//c=(a%b,a/b);
+	//printf("%d\n",c);
+	////putchar(a);
+	////putchar(b);
+	////putchar(c);
+	//c1 = 'A';
+	//c2=c1+32;
+	//printf("%c\n",c2);
+	//printf("%d\n",c2);
+	//return 0;///
+////}
+//#include<stdio.h>
+//int main()
+//{
+	//int c1,c2;
+	//c1=97;c2=98;
+	//printf("c1=%cc2=%c",c1,c2);
+	//printf("c1=%dc2=%d",c1,c2);
+
+	//return 0;
+
+
+
+
+
+//}
+//#include<stdio.h>
+//int main()
+//{
+	//char c1='C',c2='h',c3='i',c4='n',c5='a';
+	//c1=c1+4;
+	//c2=c2+4;
+	//c3=c3+4;
+	//c4=c4+4;
+	//c5=c5+4;
+	//putchar(c1);
+	//putchar(c2);
+	//putchar(c3);
+	//putchar(c4);
+	//putchar(c5);
+	//putchar('\n');
+	//printf("%c%c%c%c%c%c\n",c1,c2,c3,c4,c5);
+
+
+	//return 0;
+
+//}
+//#include<stdio.h>
+//int main()
+//{
+	//double r,h,C,S1,S2,V1,V2,d;
+	//d=3.14;
+	//scanf("%f%f",&r,&h);
+	//C=2*d*r;
+
+	//S1=d*r*r;
+	//S2=4*d*r*r;
+	//V1=4*d*r*r*r/3;
+	//V2=S1*h;
+	//V2=d*r*r*h;
+	//printf("圆周长为%7.2f\n圆面积为%7.2f\n圆球表面积为%7.2f\n圆球体积为%7.2f\n",C,S1,S2,V1);
+	//return 0;
+
+//}
+//#include<stdio.h>
+
+//int main()
+//{  char a,b;
+  // a=getchar();
+   //b=getchar();
+  // putchar(a);
+  // putchar(b);
+  // putchar('\n');
+  // return 0;
+//}
+//#include<stdio.h>
+//#include<math.h>
+//int main()
+//{
+	//double a,b,c,q,area;
+	//a=3.67;
+	//b=5.43;
+	//c=6.21;
+	//q=(a+b+c)/2;
+	//area=sqrt(q*(q-a)*(q-b)*(q-c));
+	//printf("area=%f\t",area);
+	//return 0;
+
+//}
+//#include<stdio.h>
+//int main()
+//{
+	//int i=1;
+	//while(i<=10)
+	//{
+	//printf("%d\n",i++);
+	//}
+	//return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//int i=1;
+//while(i<=10)
+//{
+//if(i!=5) printf("%d\n",i++);
+//}
+//return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+	//float a,b,c;
+	//scanf("%f%f",&a,&b);
+	//c=a+b;
+	//printf("%4.2f",c);//%A.Bf的含义为A为该数字所占位数    .表示小数点   B表示所保留浮点数小数点后几位
+//}
+/////四个数大小的比较
+///#include<stdio.h>
+//int main()
+//{
+	//int a,b,c,d;
+	//scanf("%d%d%d%d",&a,&b,&c,&d);
+	//if(a>b)a=a;else a=b;
+	//if(a>c)a=a;else a=c;
+	//if(a>d)a=a;else a=d;
+	///printf("%d",a);
+	//return 0;
+
+
+//}
+//问题：输出结果为何是0.000000
+//#include<stdio.h>
+//int main()
+//{
+	//int a;
+	//a=1;
+	//printf("%f",a);
+
+
+//}
+//问题：输出结果为0
+//#include<stdio.h>
+//int main()
+//{
+	//float a=1.0;
+	//printf("%d\n",a);
+	//return 0;
+//}
+//训练：1与2的和
+//#include<stdio.h>
+//int main()
+//{
+	//int a=1,b=2;
+	//printf("%d",a+b);
+	//return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+	//int i=1,j=1;
+	//while(i<=10)
+//{
+	//j*=i;
+	//printf("%d",j);
+//}
+	//return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//int a,b=0;	
+//for(a=1;a<=100;a++){
+//b+=a;
+//}
+//printf("%d\n",b);
+//return 0;
+//}
+//#include<stdio.h>
+	//int main(void)
+	//{  
+	  /*********Begin*********/
+	  //double r,h,c1,Sb,Vb,pi;
+	  //scanf("%f%f",&r,&h);
+	  //pi=3.14;
+	  //c1=2*pi*r;
+	  //Sb=4*pi*r*r;
+	  //Vb=pi*r*r*h;
+	  //printf("c1=%.2f\nSb=%.2f\nVb=%.2f\n",c1,Sb,Vb);
+
+	  
+	  /*********End**********/ 
+       //return 0;
+	//}
+//#include<stdio.h>
+//int main()
+//{
+	//float a=7.4;
+	//printf("%.2f",a);
+	//return 0;
+//}
+//#include<stdio.h>
+	//int main(void)
+	//{  
+	  /*********Begin*********/
+	  //int a,b,c,d,e,f,g;
+	  //printf("请输入三个整数：");
+	  //scanf("%d %d %d",&a,&b,&c);
+	  //if(a>b)f=a,e=b;else f=b,e=a;////三个数由小到大输出
+	  //if(f>c)f=f,d=c;else d=f,f=c;
+	  //if(e>d)e=e,d=d;else g=d,d=e,e=g;
+	  //printf("从小到大排列：%d，%d,%d",d,e,f);
+	  /*********End**********/ 
+       //return 0;
+	//}
+//#include<stdio.h>
+	//int main(void)
+	//{  
+	  /*********Begin*********////分段函数的计算
+	  //float x,y;
+	  //scanf("%f",&x);
+	  //if(x<0&&x!=-3)y=x*x+x-6;
+	  //else if(x>=0&&x<=10&&x!=2&&x!=3)y=x*x-5*x+6;
+	  //else y=x*x-x-1;
+	  //printf("%.3f",y);
+	  /*********End**********/ 
+       //return 0;
+	//}
+////#百分制等级评定
+////#include<stdio.h>
+	//int main(void)
+	//{  
+	  /*********Begin*********/
+	  
+	  //int x;
+	  //scanf("%d",&x);
+	  //if(x>=90&&x<=100)printf("%c",'A');
+	  //else if(x>=80&&x<=89)printf("%c",'B');
+	  //else if(x>=70&&x<=79)printf("%c",'C');
+	  //else if(x>=60&&x<=69)printf("%c",'D');
+	  //else if(x>=0&&x<=59)printf("%c",'E');
+	  //else printf("Score is error!");
+	  
+	  /*********End**********/ 
+       //return 0;
+	//}
+/////闰年的判断
+//#include<stdio.h>
+	//int main(void)
+	//{  
+	  /*********Begin*********/
+	  //int x;
+	  //scanf("%d",&x);
+	  //if(x%4==0&&x%100!=0)printf("%d 是闰年！",x);
+	  //else if(x%400==0)printf("%d 是闰年！",x);
+	  //else printf("%d 不是闰年！",x);
+	  /*********End**********/ 
+       //return 0;
+	//}
+////
